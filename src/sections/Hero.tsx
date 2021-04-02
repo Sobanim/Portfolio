35d38f2782668fe034f9f8ab1b9dae91c5600733
Dmytro Soboliev <sobolev.dmirtiy@gmail.com>
2021-04-02 12:37:47 +0200

    Section for skills working

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,5 +1,55 @@
+import React from 'react';
+import Image from 'next/image';
+
+type Info = {
+  label: string;
+  link?: string;
+};
+
+const infos: Info[] = [
+  {
+    label: 'sobolev.dmirtiy@gmail.com',
+  },
+  {
+    label: '+421 951 125 682',
+  },
+  {
+    label: 'Github',
+    link: 'https://github.com/Sobanim?tab=repositories',
+  },
+];
+
 function Hero() {
-  return <div />;
+  return (
+    <div>
+      <Image src="/images/harold-profile.jpg" width="400px" height="400px" />
+      <h2 className="text-6xl">Dmytro Soboliev</h2>
+      <div>
+        {infos.map((info, index) => {
+          const hasPipe = index !== infos.length - 1;
+          return (
+            <InfoBarItem key={index} hasPipe={hasPipe}>
+              {info.label}
+            </InfoBarItem>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+type Props = {
+  children: React.ReactNode;
+  hasPipe: boolean;
+};
+
+function InfoBarItem({ children, hasPipe }: Props) {
+  return (
+    <span>
+      {children}
+      {hasPipe && ` | `}
+    </span>
+  );
 }
 
 export default Hero;
